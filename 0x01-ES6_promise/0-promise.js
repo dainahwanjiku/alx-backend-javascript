99d3401751331fda4d8cd4489f0fdc47cf0225c4
dainahwanjiku <deeshish2017@gmail.com>
2022-12-21 22:43:30 +0000

    getResponsefromAPI

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,4 +1,6 @@
 export default getResponseFromAPI;
+  /* return a promise using the prototype function */
 function getResponseFromAPI() {
+  /* return a promise using the prototype function */
   return Promise.resolve({});
 }
