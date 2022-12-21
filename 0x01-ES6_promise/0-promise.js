ab68db23fb0cfc1c3c22e6267331c57fedf3c6b3
dainahwanjiku <deeshish2017@gmail.com>
2022-12-21 21:34:58 +0000

    all files

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,2 +1,10 @@
-let myPromise = new Promise(function getResponseFromAPI(resolve, reject));
-return new Promise();
+//let myPromise = new Promise(function getResponseFromAPI(resolve, reject));
+//return new Promise();
+const getResponseFromAPI = new Promise((resolve, reject) => {
+// We call resolve(...) when what we were doing asynchronously was successful, and reject(...) when it failed.
+  // In this example, we use setTimeout(...) to simulate async code.
+  // In reality, you will probably be using something like XHR or an HTML API.
+  setTimeout(() => {
+    resolve("Success!"); // Yay! Everything went well!
+  }, 250);
+});
