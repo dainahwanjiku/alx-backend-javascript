3e142f01e239df1a4dceeb7f5db2bb6f7e628eab
dainahwanjiku <deeshish2017@gmail.com>
2022-12-21 22:30:27 +0000

    getResponsefromAPI

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,9 @@
 //let myPromise = new Promise(function getResponseFromAPI(resolve, reject));
 //return new Promise();
-const getResponseFromAPI = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("Success!"); // Yay! Everything went well!
-  }, 250);
-});
+//const getResponseFromAPI = new Promise((resolve, reject) => {
+  //setTimeout(() => {
+    //resolve("Success!"); // Yay! Everything went well!
+  //}, 250);
+//});
+
+const response = getResponseFromAPI((resolve, reject));
