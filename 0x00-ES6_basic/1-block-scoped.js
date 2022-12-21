fc86ca2507501b77b6347247da3047999c084db4
dainahwanjiku <deeshish2017@gmail.com>
2022-12-21 21:47:58 +0000

    block-scoped

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,10 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
+    /*use the const */
     const task = true;
     const task2 = false;
+    /*use the const */
   }
 
   return [task, task2];
