47cff6f42ac079dbb4a7cf9e02e47ac12366668e
dainahwanjiku <deeshish2017@gmail.com>
2022-12-22 01:10:26 +0000

    then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,3 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({ status: 200, body: 'Success' });
-    } else {
-      reject(new Error());
-    }
-  });
+  return promise.then(() => ({ status: 200, body: 'success' })).catch(() => Error()).finally(() => { console.log('Got a response from the API'); });
 }
