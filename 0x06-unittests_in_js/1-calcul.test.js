dfc35336a42012695736792bae1a13438e22e97d
dainah <99591839+dainahwanjiku@users.noreply.github.com>
2023-02-27 05:00:27 +0300

    Update 1-calcul.test.js

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,24 +1,22 @@
 const assert = require('assert');
 const { it, describe } = require("mocha");
-const calculateNumber = require('./0-calcul.js');
+const calculateNumber = require('./1-calcul.js');
 
 describe('calculateNumber', function() {
-    it('should return 4', function() {
-        assert.strictEqual(calculateNumber(1, 3), 4);
-    });
-    it('should return 5', function() {
-        assert.strictEqual(calculateNumber(1, 3.7), 5);
-    });
+
     it('should return 6', function() {
-        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+        assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
     });
-    it('should return 5', function() {
-        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    it('should return -4', function() {
+        assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    });
+    it('should return 0.2', function() {
+        assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    });
+    it('should return Error', function() {
+        assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
     });
     it('should return 0', function() {
-        assert.strictEqual(calculateNumber(-1, 1), 0);
-    });
-    it('should return 0', function() {
-        assert.strictEqual(calculateNumber(-1.4, 1.5), 1);
+        assert.strictEqual(calculateNumber('DIVIDE', 0, 4.5), 0);
     });
 });
