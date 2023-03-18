736667046a0dc36298ebf0dc08c1294ad5441d64
dainah <99591839+dainahwanjiku@users.noreply.github.com>
2023-03-18 16:48:36 +0300

    Update 7-getBudgetObject.js

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,5 +1,9 @@
 export default function getBudgetObject(income, gdp, capita) {
-  const budget = {\n income,\n gdp,\n capita,\n };
+  const budget = {
+    income,
+    gdp,
+    capita,       
+  }
 
   return budget;
 }
