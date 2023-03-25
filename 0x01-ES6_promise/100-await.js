329bb706dedf9456038c2086628326e11d50be46
dainah <99591839+dainahwanjiku@users.noreply.github.com>
2023-03-25 16:26:46 +0300

    await.js

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,14 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  let res = {};
-
   try {
-    const photo = await uploadPhoto();
-    const user = await createUser();
-    res = { photo, user };
-  } catch (err) {
-    res = { photo: null, user: null };
+    return { photo: await uploadPhoto(), user: await createUser() };
+  } catch (e) {
+    return { photo: null, user: null };
   }
-  return res;
 }
